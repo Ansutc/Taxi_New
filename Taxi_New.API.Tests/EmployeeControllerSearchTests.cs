ad6f94ec4a0c4d437b44a2f46fcacf4a2aace330
Ansutc <1284kjg@mail.ru>
2021-01-26 09:42:56 +0300

    first commit

---
+++ Taxi_New.API.Tests/EmployeeControllerSearchTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Taxi_New.API.Controllers;
+
+namespace Taxi_New.API.Tests
+{
+public class EmployeeControllerSearchTests
+    {
+    [Fact]
+        public void ItExists()
+        {
+        var controller = new EmployeeController();
+        }
+    }   
+}
---
+++ Taxi_New.API/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+namespace Taxi_New.API.Controllers
+    {  
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+        {
+    }
+}
